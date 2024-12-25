c54cf4cb15578e1b41b4f824f254b96a4c6ffa5b
murzagalin <azamat.em@gmail.com>
2024-12-25 19:02:27 +0100

    Added transfer request

--- api/main_test.go
+++ api/main_test.go
@@ -1,15 +1,12 @@
 package api
 
 import (
-	"database/sql"
 	"os"
 	"testing"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 )
-
-var testDb *sql.DB
 
 func TestMain(m *testing.M) {
 	gin.SetMode(gin.TestMode)
--- api/server.go
+++ api/server.go
@@ -2,6 +2,8 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 	db "github.com/murzagalin/go_playground/db/sqlc"
 )
 
@@ -17,11 +19,16 @@ func NewServer(store db.Store) *Server {
 		store:  store,
 		router: router,
 	}
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
 
 	// Add routes to the server
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccounts)
+
+	router.POST("/transfers", server.createTransfer)
 
 	return server
 }
