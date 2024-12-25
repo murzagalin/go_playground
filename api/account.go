5a455c08e57a860ad369743e5e466a5221226ac5
murzagalin <azamat.em@gmail.com>
2024-12-25 20:32:40 +0100

    Distinguish errors on account creation

--- api/account.go
+++ api/account.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
 	db "github.com/murzagalin/go_playground/db/sqlc"
 )
 
@@ -28,6 +29,14 @@ func (server *Server) createAccount(ctx *gin.Context) {
 
 	account, err := server.store.CreateAccount(ctx, arg)
 	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "foreign_key_violation", "unique_violation":
+				ctx.JSON(http.StatusBadRequest, errorResponse(err))
+				return
+			}
+		}
+
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
